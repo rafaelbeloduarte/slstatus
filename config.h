014ca53feadc925215bfac64fad57a972d5bc8bd
Rafael <rafaelbeloduarte@protonmail.com>
2021-11-18 22:29:36 +0000

    Primeiro commit

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* interval between updates (in ms) */
-const unsigned int interval = 1000;
+const unsigned int interval = 60000;
 
 /* text to show if no value can be retrieved */
 static const char unknown_str[] = "n/a";
@@ -65,5 +65,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ keymap, 	"%s", 	NULL },
+	{ separator, 	"%s", 	" " },
+	{ vol_perc, 	"Vol:%s%%",   "/dev/mixer" },
+	{ separator, 	"%s", 	" " },
+	{ datetime, 	"%s",   "%a %d/%m %H:%M" },
 };
