93e7ea1d1a3d9f90988121aeb34b52e586919b1a
Rafael Belo Duarte <rafaelbeloduarte@protonmail.com>
2023-07-02 09:43:54 -0300

    changed cpu temp monitor to 'sensors'

--- config.h
+++ config.h
@@ -75,7 +75,7 @@ static const struct arg args[] = {
 	{ vol_perc, 	"%s%% ",       "/dev/mixer" },
   { netspeed_rx, "D %s ",     "enp0s31f6"},
   { netspeed_tx, "U %s ",     "enp0s31f6"},
-  { temp,       "CPU %sºC ",       "/sys/class/hwmon/hwmon2/temp1_input"},
+  { run_command,"CPU %s ",      "sensors | awk '/^Package/ {print $4}'"},
   { run_command,"GPU %sºC ",      "nvidia-smi --query-gpu=temperature.gpu --format=csv,noheader"},
 	{ datetime, 	"%s",         "%a %d/%m %H:%M" },
 };
